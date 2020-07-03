d8850812cc4c0f8796fb4240fee070160769c048
jjdcabasolo <abasolojohnjourish@gmail.com>
2020-07-03 16:00:07 +0800

    chore: add linkedin and github links

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,1 +1,6 @@
-export default 'https://www.figma.com/file/hDBEIkgn22vA4HkkLJ31gt/Draft-Mockup-COVIDAppTracker?node-id=0%3A1';
+export const figmaLink =
+  'https://www.figma.com/file/hDBEIkgn22vA4HkkLJ31gt/%5BPublic%5D-%5BComponentMockup%5D-COVID-Tracker-App';
+
+export const linkedInLink = 'https://www.linkedin.com/in/jjdcabasolo/';
+
+export const githubLink = 'https://github.com/jjdcabasolo';
