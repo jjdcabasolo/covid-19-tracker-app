d653ac552faec093ea52f46c4811143019c9aee3
jjdcabasolo <abasolojohnjourish@gmail.com>
2020-07-03 15:43:31 +0800

    chore: add/update app descriptions

--- src/constants/descriptions.js
+++ src/constants/descriptions.js
@@ -1,25 +1,52 @@
 import { html } from 'lit-element';
 
-import figmaLink from './links';
+import { figmaLink, linkedInLink } from './links';
 
 export default [
   html`
     <div class="small-text primary-text">
       Checkout the outdated
-      <a href="${figmaLink}">component mockup</a>
-      🖼 for this app!
+      <a href=${figmaLink}>component mockup at Figma</a> for this app!
     </div>
   `,
 
   html`
     <div class="small-text primary-text">
-      Night mode 🌓 depends on your device's theme preference. Try toggling it in settings!
+      Night mode 🌓 depends on your device's theme preference. Try toggling it!
     </div>
   `,
 
   html`
     <div class="small-text primary-text">
-      Animations will be implemented soon 😉
+      Shoutout to my design consultants/app testers: Kharisa, Jemil, Camille,
+      Blessy, and RC 😍
+    </div>
+  `,
+
+  html`
+    <div class="small-text primary-text">
+      Country pinning 📌 will be implemented soon!
+    </div>
+  `,
+
+  html`
+    <div class="small-text primary-text">
+      Noticed something wrong? Send it here at my
+      <a href="mailto:abasolojohnjourish@gmail.com">email</a>.
+    </div>
+  `,
+
+  html`
+    <div class="small-text primary-text">
+      Here's my <a href=${linkedInLink}>LinkedIn</a> account. Nothing, just
+      saying. 😉
+    </div>
+  `,
+
+  html`
+    <div class="small-text primary-text">
+      Noticed something wrong? Send it here at my
+      <a href="mailto:abasolojohnjourish@gmail.com">email</a>.
     </div>
   `,
 ];
