af4169fcdff6dc71fd1d7d881100a61b9bc6cb50
jjdcabasolo <abasolojohnjourish@gmail.com>
2020-07-03 16:55:58 +0800

    refactor: main app structure

--- src/COVID19TrackerApp.js
+++ src/COVID19TrackerApp.js
@@ -3,12 +3,16 @@ import { nothing } from 'lit-html';
 
 import './components/app-description';
 import './components/country-list-connected';
-import './components/utility-banner';
+import './components/utility-panel';
 import './components/worldwide-banner';
 
 import darkThemeStyles from './styles/dark-theme-styles';
 import flexboxStyles from './styles/flexbox-styles';
 import fontStyles from './styles/font-styles';
+
+const APP_TITLE = html`<h1 class="primary-text">COVID-19 Tracker</h1>`;
+const MOBILE = 600;
+const TABLET = 1000;
 
 export default class COVID19TrackerApp extends LitElement {
   static get styles() {
@@ -19,21 +23,35 @@ export default class COVID19TrackerApp extends LitElement {
         main {
           margin: auto;
           max-width: 1000px;
-          padding: 72px 24px;
+          padding: 72px 24px 0 24px;
+        }
+        h1 {
+          margin: 0;
+        }
+        .content {
+          margin: 16px 0;
+        }
+        .left-panel {
+          flex-basis: 75%;
+          flex-direction: column;
+          flex-grow: 0;
+          max-width: 75%;
+        }
+        .right-panel {
+          flex-basis: 25%;
+          flex-grow: 0;
+          max-width: 25%;
+          position: relative;
+        }
+        .right-panel-container {
+          position: fixed;
+          padding-left: 16px;
+          max-width: 15vw;
         }
         @media screen and (max-width: 600px) {
           main {
             padding-bottom: 0;
           }
-        }
-        .content {
-          margin: 24px 0;
-        }
-        .content:last-child {
-          margin-bottom: 0;
-        }
-        .world-banner {
-          margin-bottom: 8px;
         }
       `,
       darkThemeStyles,
@@ -42,11 +60,13 @@ export default class COVID19TrackerApp extends LitElement {
 
   static get properties() {
     return {
+      config: { type: Object },
+      coverage: { type: Map },
       error: { type: String },
+      isDesktop: { type: Boolean },
       isLoading: { type: Boolean },
       isMobile: { type: Boolean },
       query: { type: String },
-      config: { type: Object },
       worldwide: { type: Object },
     };
   }
@@ -54,82 +74,109 @@ export default class COVID19TrackerApp extends LitElement {
   constructor() {
     super();
 
-    this.error = '';
-    this.isLoading = true;
-    this.isMobile = window.innerWidth < 600;
-    this.query = '';
     this.config = {
-      count: 'cases-desc',
-      coverage: 'country',
       countryCount: 0,
+      filter: 'worldwide',
+      sort: 'cases-desc',
     };
+    this.coverage = new Map();
+    this.error = '';
+    this.isDesktop = window.innerWidth < TABLET;
+    this.isLoading = true;
+    this.isMobile = window.innerWidth < MOBILE;
+    this.query = '';
     this.worldwide = {};
   }
 
   connectedCallback() {
     super.connectedCallback();
-    window.addEventListener('resize', () => COVID19TrackerApp.handleResize(this));
+    window.addEventListener('resize', () =>
+      COVID19TrackerApp.handleResize(this)
+    );
   }
 
   disconnectedCallback() {
-    window.removeEventListener('resize', () => COVID19TrackerApp.handleResize(this));
+    window.removeEventListener('resize', () =>
+      COVID19TrackerApp.handleResize(this)
+    );
     super.disconnectedCallback();
   }
 
   render() {
-    const appTitle = html`<h1 class="primary-text">COVID-19 Tracker</h1>`;
-
     if (this.error !== '') {
       return html`
         <main>
-          ${appTitle}
+          ${APP_TITLE}
           <p class="primary-text">${this.error}</p>
         </main>
       `;
     }
 
+    if (this.isDesktop) {
+      return html`
+        <main>
+          ${this.renderCountryList()}
+        </main>
+        ${this.renderUtilityBanner('bottom')}
+      `;
+    }
+
     return html`
-      <main>
-        <div class="content">
-          ${appTitle}
+      <main class="container">
+        <div class="item left-panel">
+          ${this.renderCountryList()}
         </div>
-
-        <div class="content">
-          <app-description></app-description>
-        </div>
-
-        ${this.renderUtilityBanner('top')}
-
-        <div class="content">
-          <country-list-connected
-            ?isMobile="${this.isMobile}"
-            @handle-error="${this.handleError}"
-            @handle-fetching="${this.handleFetching}"
-            @set-config="${this.setConfig}"
-            @handle-worldwide-update="${this.handleWorldwideUpdate}"
-            @update-is-fetching="${this.updateIsFetching}"
-            count="${this.config.count}"
-            coverage="${this.config.coverage}"
-            query="${this.query}"
-          ></country-list-connected>
+        <div class="item right-panel">
+          <div class="right-panel-container">
+            <utility-panel
+              .worldwide=${this.coverage.get(this.config.filter)}
+              ?isLoading=${this.isLoading}
+              ?isMobile=${this.isMobile}
+              @handle-search-query=${this.handleSearchQuery}
+              @set-config=${this.setConfig}
+              filter=${this.config.filter}
+              sort=${this.config.sort}
+            ></utility-panel>
+          </div>
         </div>
       </main>
+    `;
+  }
 
-      ${this.renderUtilityBanner('bottom')}
+  renderCountryList() {
+    return html`
+      <div class="content">
+        ${APP_TITLE}
+      </div>
+
+      <div class="content">
+        <app-description></app-description>
+      </div>
+
+      ${this.isDesktop ? this.renderUtilityBanner('top') : null}
+
+      <div class="content">
+        ${this.renderWorldwideBanner()}
+        <country-list-connected
+          ?isMobile=${this.isMobile}
+          @handle-coverage-update=${this.handleCoverageUpdate}
+          @handle-error=${this.handleError}
+          @update-is-fetching=${this.updateIsFetching}
+          filter=${this.config.filter}
+          query=${this.query}
+          sort=${this.config.sort}
+        ></country-list-connected>
+      </div>
     `;
   }
 
   renderWorldwideBanner() {
-    if (Object.keys(this.worldwide).length > 0) {
+    if (this.coverage.get(this.config.filter)) {
       return html`
         <worldwide-banner
-          ?isMobile="${this.isMobile}"
-          cases="${this.worldwide.cases.total}"
-          deaths="${this.worldwide.deaths.total}"
-          newCases="${this.worldwide.cases.new}"
-          newDeaths="${this.worldwide.deaths.new}"
-          recovered="${this.worldwide.cases.recovered}"
-          time="${this.worldwide.time}"
+          .coverage=${this.coverage.get(this.config.filter)}
+          ?isMobile=${this.isMobile}
+          activeFilter=${this.config.sort}
         ></worldwide-banner>
       `;
     }
@@ -138,22 +185,26 @@ export default class COVID19TrackerApp extends LitElement {
   }
 
   renderUtilityBanner(location) {
+    if (location === 'top' && this.isMobile) return null;
+    if (location === 'bottom' && !this.isMobile) return null;
+
     return html`
-      <utility-banner
-        .countryCount="${this.config.countryCount}"
-        ?isLoading="${this.isLoading}"
-        ?isMobile="${this.isMobile}"
-        @handle-search-query="${this.handleSearchQuery}"
-        count="${this.config.count}"
-        coverage="${this.config.coverage}"
-        location="${location}"
-      ></utility-banner>
+      <utility-panel
+        .worldwide=${this.coverage.get(this.config.filter)}
+        ?isLoading=${this.isLoading}
+        ?isMobile=${this.isDesktop}
+        @handle-search-query=${this.handleSearchQuery}
+        @set-config=${this.setConfig}
+        filter=${this.config.filter}
+        sort=${this.config.sort}
+      ></utility-panel>
     `;
   }
 
   static handleResize(context) {
     // eslint-disable-next-line no-param-reassign
-    context.isMobile = window.innerWidth < 600;
+    context.isMobile = window.innerWidth < MOBILE;
+    context.isDesktop = window.innerWidth < TABLET;
   }
 
   handleSearchQuery({ detail }) {
@@ -170,10 +221,10 @@ export default class COVID19TrackerApp extends LitElement {
     this.config = updatedSortBy;
   }
 
-  handleWorldwideUpdate({ detail }) {
-    const { worldwide } = detail;
+  handleCoverageUpdate({ detail }) {
+    const { coverage } = detail;
 
-    this.worldwide = worldwide;
+    this.coverage = coverage;
   }
 
   handleError({ detail }) {
